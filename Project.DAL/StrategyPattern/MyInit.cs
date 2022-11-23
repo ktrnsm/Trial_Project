33aec8f5d5312ef9fc5c5a35f128f4b37aeaafca
iktueren <iktuerensemih@gmail.com>
2022-11-23 23:35:48 +0300

    Strategy Pattern Applied, Dantex Cyrpt Added, DAL Level filled

---
+++ Project.DAL/StrategyPattern/MyInit.cs
@@ -0,0 +1,26 @@
+﻿using Project.DAL.Context;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.StrategyPattern
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            #region Admin
+
+            AppUser au = new AppUser();
+            au.UserName = "Smh";
+            au.Password = "";
+
+            #endregion
+
+        }
+    }
+}
